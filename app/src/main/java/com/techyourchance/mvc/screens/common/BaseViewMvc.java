643c1fa361f4104eeb496e47a31c6a8463387c7c
Alexandre Di Guida <alex@gearedapp.co.uk>
2019-02-06 13:49:10 +0000

    Extraction of BaseObservableViewMvc and BaseViewMvc

--- app/src/main/java/com/techyourchance/mvc/screens/common/BaseViewMvc.java
+++ app/src/main/java/com/techyourchance/mvc/screens/common/BaseViewMvc.java
@@ -1,4 +1,26 @@
 package com.techyourchance.mvc.screens.common;
 
-public class BaseViewMvc {
+import android.content.Context;
+import android.view.View;
+
+public class BaseViewMvc implements ViewMvc {
+    private View mRootView;
+
+    @Override
+    public View getRootView() {
+        return null;
+    }
+
+    protected  void setRootView(View rootView) {
+        mRootView = rootView;
+    }
+
+    protected  <T extends View> T findViewById(int id) {
+        return getRootView().findViewById(id);
+    }
+
+    protected Context getContext() {
+        return getRootView().getContext();
+    }
+
 }
--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.java
@@ -9,23 +9,21 @@ import android.view.ViewGroup;
 
 import com.techyourchance.mvc.R;
 import com.techyourchance.mvc.questions.Question;
+import com.techyourchance.mvc.screens.common.BaseObservableViewMvc;
+import com.techyourchance.mvc.screens.common.BaseViewMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionsListViewMvcImpl implements QuestionsRecyclerAdapter.Listener, QuestionsListViewMvc {
+public class QuestionsListViewMvcImpl extends BaseObservableViewMvc<QuestionsListViewMvc.Listener> implements QuestionsRecyclerAdapter.Listener, QuestionsListViewMvc {
 
 
     private RecyclerView mRecyclerQuestions;
     private QuestionsRecyclerAdapter mAdapter;
 
 
-    private final View mRootView;
-
-    private  final List<Listener> mListeners = new ArrayList<>(1);
-
     public QuestionsListViewMvcImpl(LayoutInflater inflater, ViewGroup parent) {
-        mRootView = inflater.inflate(R.layout.layout_questions_list, parent, false);
+        setRootView(inflater.inflate(R.layout.layout_questions_list, parent, false));
 
         mRecyclerQuestions = findViewById(R.id.recycler_questions);
         mRecyclerQuestions.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -33,32 +31,10 @@ public class QuestionsListViewMvcImpl implements QuestionsRecyclerAdapter.Listen
         mRecyclerQuestions.setAdapter(mAdapter);
     }
 
-    @Override
-    public void registerListener(Listener listener) {
-        mListeners.add(listener);
-    }
-
-    @Override
-    public void unregisterListener(Listener listener) {
-        mListeners.remove(listener);
-    }
-
-    private Context getContext() {
-        return getRootView().getContext();
-    }
-
-    private <T extends View> T findViewById(int id) {
-        return getRootView().findViewById(id);
-    }
-
-    @Override
-    public View getRootView() {
-        return  mRootView;
-    }
 
     @Override
     public void onQuestionClicked(Question question) {
-        for (Listener list: mListeners) {
+        for (Listener list: getListeners()) {
             list.onQuestionClicked(question);
         }
     }
